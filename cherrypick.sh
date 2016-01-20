e1988795b4fea5117f2665fa287f6b3af9f457b1
SilversurferCH <Kev.Ut@gmx.de>
2016-01-20 20:49:55 +0100

    neues cherry-pick Skript

--- cherrypick.sh
+++ cherrypick.sh
@@ -1,27 +1,22 @@
 #!/bin/sh
 # 
 ###############################################################################################
-# Skript zum einfachen Übertragen von allgemeinen Änderungen in den Branches
+# Dieses Skript Ã¼bertrÃ¤gt Ã„nderungen aus dem Master-Branch in die DomÃ¤nen
 # 
-# Dieses Skript überträgt die Änderungen die im Branch master commited werden. 
-#
-# Das Script benötigt die folgenden Kommandozeilenparameter:
-# - commit-hash
-#
+# Das Script benÃ¶tigt die folgenden Kommandozeilenparameter:
+# - commit-hash 
+#  
 ###############################################################################################
 
-# Bei Ausführung auf dem Buildserver ist die Variable $WORKSPACE gesetzt 
-# andernfalls wird das aktuelle Verzeichnis verwendet  
-
-git checkout Domäne-01
+git checkout DomÃ¤ne-01
 git cherry-pick $1
-git checkout Domäne-02
+git checkout DomÃ¤ne-02
 git cherry-pick $1
-git checkout Domäne-03
+git checkout DomÃ¤ne-03
 git cherry-pick $1
-git checkout Domäne-04
+git checkout DomÃ¤ne-04
 git cherry-pick $1
-git checkout Domäne-05
+git checkout DomÃ¤ne-05
 git cherry-pick $1
-git checkout Domäne-06
+git checkout DomÃ¤ne-06
 git cherry-pick $1
